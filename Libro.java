a4a8ac898918270d3c995ca7e68aab9b08a01508
Marcos <martinezdiezmarcos7@gmail.com>
2022-12-16 10:11:21 +0100

    Ahora pone los detalles

--- Libro.java
+++ Libro.java
@@ -17,33 +17,43 @@ public class Libro {
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numeroDePaginas)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas= numeroDePaginas;
     }
-    
+
     public String getAutor(){
         return autor;
     }
-    
+
     public String getTitulo(){
         return titulo;
     }
-    
+
     public void imprimirTitulo(){
         System.out.println("##################");
         System.out.println("Titulo: " + titulo);
         System.out.println("##################");
     }
-    
+
     public void imprimirAutor(){
         System.out.println("##################");
         System.out.println("Autor: " + autor);
         System.out.println("##################");
     }
-    
+
     public int getNumeroPaginas(){
         return numeroPaginas;
     }
+
+    public void imprimirDetalles(){
+        System.out.println("Titulo: "+titulo+" Autor: "+autor+" Paginas: "+numeroPaginas);
+    }
+
+    public String getDetalles(){
+        return("Titulo: "+titulo+" Autor: "+autor+" Paginas: "+numeroPaginas);
+    }
 }
+
